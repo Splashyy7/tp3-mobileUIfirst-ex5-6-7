b6fc8270451c0ac42c69ef4d65c59fce9fa83b82
João Pedro Oliveira Gritz de Oliveira <135381904+Splashyy7@users.noreply.github.com>
2024-09-19 10:53:16 -0300

    first commit

---
+++ src/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+  return (
+    <main className="main">
+      <section className="text-section">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere architecto illo assumenda rem vel amet est fuga ut impedit deserunt, sapiente molestias eius, placeat aspernatur cumque aperiam. Suscipit, neque vel.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, dolor dolore. Tempore dolorem quod id, hic corrupti nobis optio reiciendis quos ex dignissimos laboriosam dolores porro quam illum quae voluptas!</p>
+      </section>
+      <section className="image-section">
+        <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-youtube-social-media-round-icon-png-image_6315993.png" alt="Imagem 1" />
+        <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-youtube-social-media-round-icon-png-image_6315993.png" alt="Imagem 2" />
+      </section>
+    </main>
+  );
+};
+
+export default Main;
